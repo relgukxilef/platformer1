70d61174fe7306c82d52b608cfe14484732a5272
Felix Kugler <relgukxilef@gmail.com>
2020-09-25 01:13:25 +0200

    Implement per fragment shading and gamma correction for player

--- shader/player_fragment.glsl
+++ shader/player_fragment.glsl
@@ -1,7 +1,12 @@
 #version 140
 
-in vec3 vertex_color;
+//in vec3 vertex_color;
+in vec3 world_normal;
 
 void main(void) {
-    gl_FragColor = vec4(vertex_color, 1.0);
+    vec3 vertex_color = vec3(0.5 + 0.5 * dot(
+         world_normal,
+         normalize(vec3(0.25, 0.5, 1))
+     ));
+    gl_FragColor = vec4(pow(vertex_color, vec3(1.0 / 2.2)), 1.0);
 }
